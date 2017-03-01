ba3e455d6d2b81f0a1b7bb17aeed51364658387a
Michał Dopieralski <dopieralskimichal@gmail.com>
2017-03-01 09:38:45 +0100

    chore(protractor updated config):

---
+++ protractor.config.js
@@ -0,0 +1,30 @@
+const SpecReporter = require('jasmine-spec-reporter');
+
+exports.config = {
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  baseUrl: 'http://localhost:3000/',
+  specs: [
+    'src/e2e-tests/**/*.js',
+  ],
+  exclude: [],
+  framework: 'jasmine2',
+  allScriptsTimeout: 110000,
+  jasmineNodeOpts: {
+    showTiming: true,
+    showColors: true,
+    isVerbose: false,
+    includeStackTrace: false,
+    defaultTimeoutInterval: 400000
+  },
+  directConnect: true,
+  capabilities: {
+    browserName: 'firefox',
+    version: '',
+    platform: 'ANY'
+  },
+  onPrepare: function() {
+    browser.ignoreSynchronization = true;
+
+    jasmine.getEnv().addReporter(new SpecReporter());
+  }
+};
